bd7057585fc1ec82e468a603c8f4f37e96c6beeb
jesmigel <jesmigel.developer@gmail.com>
2021-03-13 02:28:31 +1100

    ADD: sudo apt

--- scripts/bootstrap.packer.sh
+++ scripts/bootstrap.packer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y curl qemu-system qemu-kvm
+sudo apt-get update -y
+sudo apt-get install -y curl qemu-system qemu-kvm
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install packer
